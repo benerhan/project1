4a0621b801bbedbf32d52ac66496bc25e9374156
Erhan Evcin <erhan@Erhans-MacBook-Pro.local>
2025-03-11 08:25:31 +0100

    Finish Project1

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("new/", views.new_page, name="new_page"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("random/", views.random_page, name="random_page"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 import markdown2
-from django.shortcuts import render
+import random
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -23,3 +24,82 @@ def entry(request, title):
     return render(
         request, "encyclopedia/entry.html", {"title": title, "content": html_content}
     )
+
+
+def search(request):
+    query = (
+        request.GET.get("q", "").strip().lower()
+    )  # Get the query from the GET request
+    entries = util.list_entries()  # Get the list of entries
+
+    # Check if the query matches any entry exactly
+    for entry in entries:  # Check if the query matches any entry
+        if entry.lower() == query:  # If it does
+            return redirect("entry", title=entry)  # redirect to the entry page
+
+    # Serach for entries that contain the query as a substring
+    search_result = [entry for entry in entries if query in entry.lower()]
+
+    return render(
+        request,
+        "encyclopedia/search.html",
+        {"query": query, "search_result": search_result},
+    )
+
+
+def new_page(request):
+    if request.method == "POST":  # If the form is submitted
+        title = request.POST.get(
+            "title", ""
+        ).strip()  # Get the title from the POST request
+        content = request.POST.get(
+            "content", ""
+        ).strip()  # Get the content from the POST request
+
+        # Check if the title is empty
+        if not title or not content:  # If the title is empty
+            return render(
+                request,
+                "encyclopedia/error.html",
+                {"message": "The title cannot be empty."},
+            )  # Return an error message
+
+        # Check if the title is already taken
+        if util.get_entry(title):
+            return render(
+                request,
+                "encyclopedia/error.html",
+                {"message": f"The page '{title}' already exists."},
+            )  # Return an error message
+
+        formatted_content = f"# {title}\n\n{content}"  # Format the content
+
+        util.save_entry(title, formatted_content)  # Save the entry
+
+        return redirect("entry", title=title)  # Redirect to the entry page
+
+    return render(request, "encyclopedia/new_page.html")  # Render the new page form
+
+
+def edit(request, title):
+    if request.method == "POST":  # If the form is submitted
+        content = request.POST.get(
+            "content", ""
+        ).strip()  # Get the content from the POST request
+
+        util.save_entry(title, content)  # Save the entry
+
+        return redirect("entry", title=title)  # Redirect to the entry page
+
+    content = util.get_entry(title)  # Get the content of the entry
+
+    return render(
+        request, "encyclopedia/edit.html", {"title": title, "content": content}
+    )  # Render the edit page form
+
+
+def random_page(request):
+    entries = util.list_entries()  # Get the list of entries
+    random_entry = random.choice(entries)  # Pick a random entry
+
+    return redirect("entry", title=random_entry)  # Redirect to the random entry page
